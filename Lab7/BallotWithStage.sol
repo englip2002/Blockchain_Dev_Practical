10c66e3eb9223574e50ead54a7ca2eaf54593cda
Englip2002 <englip455@gmail.com>
2023-04-04 14:40:37 +0800

    ballot

--- Lab7/BallotWithStage.sol
+++ Lab7/BallotWithStage.sol
@@ -35,7 +35,13 @@ contract Ballot {
     constructor() {
         chairperson = msg.sender;
         voters[chairperson].weight = 2;
+        stage = Stage.Init;
         startTime = block.timestamp;
+    }
+
+    function changeStateToReg() public returns (Stage){
+        stage = Stage.Reg;
+        return stage;
     }
 
     function addProposalDetail(uint256 _id, string memory _subjectProposal)
@@ -57,34 +63,39 @@ contract Ballot {
     //Instead of use if statement to do validation of chairperson,we can use CUSTOM MODIFIER
     function register(address toVoter) public validStage(Stage.Reg) {
         //the sender must be chairperson (at deploy account register)
-        if (
-            msg.sender != chairperson ||
-            voters[toVoter].voted == true
-        ) return;
+        if (msg.sender != chairperson || voters[toVoter].voted == true) return;
         voters[toVoter].weight = 1;
         voters[toVoter].voted = false;
         VoterCount++;
+        if (block.timestamp > (startTime + 30 seconds)) {
+            stage = Stage.Vote;
+            startTime = block.timestamp;
+        }
     }
 
     /// Give a single vote to proposal $(toProposal).
     // toProposal is the id of proposal
     function vote(uint8 toProposal) public validStage(Stage.Vote) {
+        //if (stage != Stage.Reg) {return;}
         Voter storage sender = voters[msg.sender];
-        if (
-            sender.voted ||
-            toProposal >= proposals.length
-        ) return;
+        if (sender.voted || toProposal >= proposals.length) return;
         sender.voted = true;
         sender.vote = toProposal;
 
         proposals[toProposal].voteCount += sender.weight;
+        
+        if (block.timestamp > (startTime + 30 seconds)) {
+            stage = Stage.Done;
+        }
     }
 
     function winningProposal()
         public
         view
         validStage(Stage.Done)
-        returns (uint8 _winningProposal) {
+        returns (uint8 _winningProposal)
+    {
+        //if(stage != Stage.Done) {return;}
         uint256 winningVoteCount = 0;
 
         for (uint8 prop = 0; prop < proposals.length; prop++)
@@ -92,25 +103,5 @@ contract Ballot {
                 winningVoteCount = proposals[prop].voteCount;
                 _winningProposal = prop;
             }
-    }
-
-    function advanceState() public {
-        if (block.timestamp > (startTime + 2 seconds)) {
-            if (stage == Stage.Init) {
-                stage = Stage.Reg;
-                return;
-            }
-
-            if (stage == Stage.Reg) {
-                stage = Stage.Vote;
-                return;
-            }
-
-            if (stage == Stage.Vote) {
-                stage = Stage.Done;
-                return;
-            }
-            return;
-        }
     }
 }
